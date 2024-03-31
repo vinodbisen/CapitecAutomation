0b462f702bd8462a7c8ac37205583d6697fb2b34
vinodbisen <vinod.bisen@icloud.com>
2024-04-01 00:25:04 +0200

    updated code
    updated code

--- SwagLabsUI/tests/checkoutoverview.spec.ts
+++ SwagLabsUI/tests/checkoutoverview.spec.ts
@@ -76,17 +76,32 @@ test.describe('Checkout: Overview screen', () => {
         await checkoutinfopage.filluserinfo("Vinod","Bisen","7550")
         await expect(page.getByText("Checkout: Overview")).toHaveText("Checkout: Overview")
         await page.waitForTimeout(1000)
+        const pdescription: string[] = ["carry.allTheThings() with the sleek, streamlined Sly Pack that melds uncompromising style with unequaled laptop and tablet protection.",
+        "A red light isn't the desired state in testing but it sure helps when riding your bike at night. Water-resistant with 3 lighting modes, 1 AAA battery included.",
+        "Get your testing superhero on with the Sauce Labs bolt T-shirt. From American Apparel, 100% ringspun combed cotton, heather gray with red bolt.",
+        "It's not every day that you come across a midweight quarter-zip fleece jacket capable of handling everything from a relaxing day outdoors to a busy day at the office.",
+        "Rib snap infant onesie for the junior automation engineer in development. Reinforced 3-snap bottom closure, two-needle hemmed sleeved and bottom won't unravel."]
+        const pprice: string[] = ["$29.99","$9.99","$15.99","$49.99","$7.99"]
         const tquantity = page.locator('#checkout_summary_container div[data-test*=inventory-item] div[data-test*=item-quantity]')
+
         for (const query of await tquantity.elementHandles()){
-            console.log(await query.textContent());
+            await expect(await query.textContent()).toEqual('1');
+
+            //console.log(await query.textContent());
         }
+    
         const tdescription = page.locator('#checkout_summary_container div[data-test*=inventory-item] div[data-test*=inventory-item-desc]')
+        var i = 0;
         for (const query of await tdescription.elementHandles()){
-            console.log(await query.textContent());
+            await expect(await query.textContent()).toEqual(pdescription[i]);
+            i = i+1;
+            // console.log(await query.textContent());
         }
+        var ii = 0;
         const tprice = page.locator('#checkout_summary_container div[data-test*=inventory-item] div[data-test*=inventory-item-price]')
         for (const query of await tprice.elementHandles()){
-            console.log(await query.textContent());
+            await expect(await query.textContent()).toEqual(pprice[ii]);
+            ii = ii+1;
         }
     })
     test('User is able to view the Payment and Shipping information on screen', async ({ page }) => {
@@ -120,7 +135,8 @@ test.describe('Checkout: Overview screen', () => {
         await checkoutinfopage.filluserinfo("Vinod","Bisen","7550")
         await expect(page.getByText("Checkout: Overview")).toHaveText("Checkout: Overview")
         await page.waitForTimeout(1000)
-        // Payment and Shipping information display
+        await expect(page.locator("div[data-test*=payment-info-value]")).toHaveText("SauceCard #31337")
+        await expect(page.locator("div[data-test*=shipping-info-value]")).toHaveText("Free Pony Express Delivery!")
     })
     test('User is able to validate the Price Total on screen', async ({ page }) => {
         homepage = new HomePage(page)
@@ -153,7 +169,20 @@ test.describe('Checkout: Overview screen', () => {
         await checkoutinfopage.filluserinfo("Vinod","Bisen","7550")
         await expect(page.getByText("Checkout: Overview")).toHaveText("Checkout: Overview")
         await page.waitForTimeout(1000)
-        // Validation for Price Total on screen
+        var sum = 0;
+        const tprice = page.locator('#checkout_summary_container div[data-test*=inventory-item] div[data-test*=inventory-item-price]')
+        for (const query of await tprice.elementHandles()){
+            var p = (await query.textContent());
+            var pp = Number(p?.replace("$",""))
+            sum = sum+pp;
+        }
+        let tval = await page.locator("div[data-test*=subtotal-label]").textContent()
+        // console.log(tval)
+        // console.log(sum)
+        var ttval = tval?.split("$")
+        // console.log(ttval[1])
+        // var ttval = tval?.replace("$","")
+        await expect(Number(ttval[1])).toBe(sum)
     })
     test('User is able to validate the Total amount ', async ({ page }) => {
         homepage = new HomePage(page)
@@ -186,7 +215,15 @@ test.describe('Checkout: Overview screen', () => {
         await checkoutinfopage.filluserinfo("Vinod","Bisen","7550")
         await expect(page.getByText("Checkout: Overview")).toHaveText("Checkout: Overview")
         await page.waitForTimeout(1000)
-        // Validation for Total amount 
+        let tval = await page.locator("div[data-test*=subtotal-label]").textContent()
+        var ttval = tval?.split("$")
+        let ttax = await page.locator("div[data-test*=tax-label]").textContent()
+        var tttax = ttax?.split("$")
+        let ttotal = await page.locator("div[data-test=total-label]").textContent()
+        var tttotal = ttotal?.split("$")
+        var gtotal = Number(ttval[1])+Number(tttax[1])
+        await expect(Number(tttotal[1])).toBe(Number(gtotal.toFixed(2)))
+
     })
     test('User is able to cancel the checkout process', async ({ page }) => {
         homepage = new HomePage(page)
